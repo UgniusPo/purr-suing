8a5bdc7d5ac5aee1393fc892e2e5d5f19c8f6f11
Ugnius Poska <ugniusposka@gmail.com>
2025-12-04 15:57:23 +0000

    added variables to track number of coins/cops/fuel/oxygen, also added to display so can be seen by player

--- purrsuing.py
+++ purrsuing.py
@@ -2,28 +2,47 @@ import pygame
 
 #Set up pygame
 pygame.init()
-screen =  pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+screen =  pygame.display.set_mode((0,0), pygame.FULLSCREEN|pygame.NOFRAME)
 clock = pygame.time.Clock()
 running = True
 dt = 0
-background = pygame.image.load('mapConcept.png')
+background = pygame.image.load('mapConcept.png').convert()
+coinCount = 0
+cropCount = 0
+oxygen = 0
+fuel = 0
 
 font = pygame.font.Font('smallCake.otf', 20)
 
+coinCountText = font.render(str(coinCount), 1, 'yellow', 'black')
+cropCountText = font.render(str(cropCount), 1, 'green', 'black')
+oxygenText = font.render(str(oxygen), 1, 'blue', 'black')
+fuelCText = font.render(str(fuel), 1, 'orange', 'black')
+
 textC = font.render('Coins: ', True, 'yellow', 'black')
 textCrop = font.render('Crops: ', True, 'green', 'black')
-textFood = font.render('Food: ', True, 'brown', 'black')
+textFuel = font.render('Fuel: ', True, 'orange', 'black')
 textOx = font.render('Oxygen: ', True, 'blue', 'black')
 
 coinsRect = textC.get_rect()
 cropRect = textCrop.get_rect()
-foodRect = textFood.get_rect()
+fuelCRect = textFuel.get_rect()
 oxRect = textOx.get_rect()
+
+coinsCountRect = coinCountText.get_rect()
+cropCountRect = cropCountText.get_rect()
+oxygenRect = oxygenText.get_rect()
+fuelRect = fuelCText.get_rect()
 
 coinsRect.center = (1450, 50)
 cropRect.center = (1450, 75)
-foodRect.center = (1450, 100)
+fuelRect.center = (1450, 100)
 oxRect.center = (1450, 125)
+
+coinsCountRect.center = (1530, 50)
+cropCountRect.center = (1530, 75)
+fuelCRect.center = (1550, 100)
+oxygenRect.center = (1530, 125)
 
 #sets player position
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
@@ -40,18 +59,22 @@ while running:
         screen.blit(player, (player_pos))
         screen.blit(textC, coinsRect)
         screen.blit(textCrop, cropRect)
-        screen.blit(textFood, foodRect)
+        screen.blit(textFuel, fuelRect)
         screen.blit(textOx, oxRect)
+        screen.blit(coinCountText, coinsCountRect)
+        screen.blit(cropCountText, cropCountRect)
+        screen.blit(fuelCText, fuelCRect)
+        screen.blit(oxygenText, oxygenRect)
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            player_pos.y -= 10
+            player_pos.y -= 5
         if keys[pygame.K_s]:
-            player_pos.y += 10
+            player_pos.y += 5
         if keys[pygame.K_a]:
-            player_pos.x -= 10
+            player_pos.x -= 5
         if keys[pygame.K_d]:
-            player_pos.x += 10
+            player_pos.x += 5
         if keys[pygame.K_ESCAPE]:   #Close game when ESC is pressed
             running = False
 
